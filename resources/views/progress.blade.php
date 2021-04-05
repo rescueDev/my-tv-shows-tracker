cc8d1ebadab981d884040b37e61a2e5ed80e7daa
sa-borgia3 <sa.borgia3@gmail.com>
2021-04-05 18:55:33 +0200

    progress fixed button delete and watched

--- resources/views/progress.blade.php
+++ resources/views/progress.blade.php
@@ -2,6 +2,15 @@
 @section('content')
 
     <div class="container-fluid">
+
+        @if(!count($shows))
+        <div class="col-12 mx-auto mt-5">
+
+            <h1 class="text-center">No shows in progress, add some please!</h1>
+
+        </div>
+        @endif
+
         <div class="mx-auto row">
             <div class="mt-3 col-sm-12  d-flex flex-wrap justify-content-evenly">
                 @foreach ($shows as $show)
@@ -9,16 +18,23 @@
                         <a href="{{route('show-serie', $show->id)}}">
                             <img src="{{ 'https://image.tmdb.org/t/p/w185' . $show->poster }}" alt="">
                         </a>
-                        <form action="{{route('delete-show', $show->id)}}" method="post">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn btn-danger">Delete</button>
-                        </form>
-                        <form action="{{route('watched-show', $show->id)}}" method="post">
-                            @csrf
-                            @method('POST')
-                            <button type="submit" class="btn btn-warning">Watched</button>
-                        </form>
+
+                        <div class="d-flex justify-content-evenly">
+
+                            <form class="mr-2" action="{{route('delete-show', $show->id)}}" method="post">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                            <form action="{{route('watched-show', $show->id)}}" method="post">
+                                @csrf
+                                @method('POST')
+                                <button type="submit" class="btn btn-warning">Watched</button>
+                            </form>
+
+
+                        </div>
+
                     </div>
                 @endforeach
             </div>
